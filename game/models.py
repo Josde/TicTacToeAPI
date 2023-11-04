588264ab4b4af1845979f028832819d109d5f9b6
Josde <contact@josde.me>
2023-11-04 13:27:02 +0100

    Implementación modelo básico

--- game/models.py
+++ game/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from utils import constants
 # Create your models here.
+
+class Game(models.Model):  
+    game_id = models.AutoField(primary_key=True)
+    player_1 = models.ForeignKey(models.User)
+    player_2 = models.ForeignKey(models.User)
+    # Podríamos separar esto en Game (la parte superior, datos sobre usuarios)
+    # y GameState (la parte inferior, estado de la partida), para hacer cosas como guardar historiales completos jugada por jugada de cada partida. 
+    # Sin embargo, lo dejo para el futuro.
+    board = models.CharField(max_length=constants.BOARD_SIZE**2)
+    turn = models.IntegerField()
