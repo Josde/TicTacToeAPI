1e107e53a775b4cc65042e2bb9c9b265fd451478
Josde <contact@josde.me>
2023-11-04 12:31:41 +0100

    Added config through .env file

--- tictactoe/settings.py
+++ tictactoe/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,11 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-36%55vpy1rt(%pu448$6q!81a_@l#^_59mmcxp+y$jgeaixsl%'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-36%55vpy1rt(%pu448$6q!81a_@l#^_59mmcxp+y$jgeaixsl%' if DEBUG else config("SECRET_KEY")
+
+
 
 ALLOWED_HOSTS = []
 
